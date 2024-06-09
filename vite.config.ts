1874fb1a02930a650a2fb0d6386eede1844a5770
dilyin9 <dilyin9@gmail.com>
2024-06-10 00:06:48 +0300

    r

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dns from 'node:dns'
 
-// dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
-  // server: {
-  //   port: 3000
-  // }
+  base: './',
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ]
+  }
 })
